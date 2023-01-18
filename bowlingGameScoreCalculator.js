06191e46a37bf2391408bddf61713f559bd604c6
sanyam9 <sanyamxsharma@gmail.com>
2023-01-19 00:55:43 +0530

    fix: modify score function to cover edge case

--- bowlingGameScoreCalculator.js
+++ bowlingGameScoreCalculator.js
@@ -1,32 +1,42 @@
-function getBowlingScore(rolls){
-    if(!Array.isArray(rolls)){
+function checkErrors(rolls) {
+    if (!Array.isArray(rolls)) {
         throw new Error('Unsupported Input Type')
     }
-    if(rolls.some(isNaN)){
+    if (rolls.some(isNaN)) {
         throw new Error('Inputs must be numbers')
     }
-    if(rolls.length<20){
+    if (rolls.length < 12) {
         throw new Error("Invalid Game")
     }
+}
+
+function getBowlingScore(rolls) {
+
+    checkErrors(rolls)
 
     let finalScore = 0
     let index = 0
-    while(index<rolls.length-1){
-        if(rolls[index]===10){
-            finalScore += 10 + rolls[index+1] + rolls[index+2]
+    while (index < rolls.length - 1) {
+        console.log(finalScore)
+        if (rolls[index] === 10) {
+            finalScore += 10 + rolls[index + 1] + rolls[index + 2]
+            if (index >= 18 || finalScore === 300)
+                break
+            index++
+            continue
         }
-        
-        let frameScore = rolls[index] + rolls[index+1]
-        if(frameScore==10){
-            finalScore+= 10 + rolls[index+2]
+
+        let frameScore = rolls[index] + rolls[index + 1]
+        if (frameScore == 10) {
+            finalScore += 10 + rolls[index + 2]
         }
-        if(frameScore<10){
+        if (frameScore < 10) {
             finalScore += frameScore
         }
-        
-        index +=2
+
+        index += 2
     }
     return finalScore
 }
-//console.log(getBowlingScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+//console.log(getBowlingScore([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
 module.exports = getBowlingScore
