b7048069f679f62085c3504ee4f0aff1d904ed1f
sanyam9 <sanyamxsharma@gmail.com>
2023-01-18 12:18:13 +0530

    feat: add functionality to check errors

--- bowlingGameScoreCalculator.js
+++ bowlingGameScoreCalculator.js
@@ -1,3 +1,11 @@
 function getBowlingScore(rolls){
-    
+    if(!Array.isArray(rolls)){
+        throw new Error('Unsupported Input Type')
+    }
+    if(rolls.some(isNaN)){
+        throw new Error('Inputs must be')
+    }
+    if(rolls.length<20){
+        throw new Error("Invalid Game")
+    }
 }
